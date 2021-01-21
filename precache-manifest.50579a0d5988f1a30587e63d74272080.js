e0c2e7b6a1115502f7a6f271a26766f765fd48dd
jmart6784 <jmartinez6784@yahoo.com>
2021-01-21 13:03:00 -0800

    Updates

---
+++ precache-manifest.50579a0d5988f1a30587e63d74272080.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8113c4c328fac2938742f702d408e123",
+    "url": "/memory-card/index.html"
+  },
+  {
+    "revision": "152b64a1566a95710f59",
+    "url": "/memory-card/static/js/2.22a3c0a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-card/static/js/2.22a3c0a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056b3b6ea69c03f77f3f",
+    "url": "/memory-card/static/js/main.d30dd09f.chunk.js"
+  },
+  {
+    "revision": "2d10a56d9bedc8f13787",
+    "url": "/memory-card/static/js/runtime-main.4cf988ce.js"
+  }
+]);
